9b2faaac5036fc28c9a59de75d9d4cf76707c884
kartik-kanaujia <kartik.kanaujia@gmail.com>
2017-07-09 03:26:15 +0530

    First Commit
    Adding this project to GitHub

---
+++ Stack(2)/Assets/Scripts/MuteButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuteButton : MonoBehaviour {
+
+	private Camera cam;
+	private AudioSource aud;
+	// Use this for initialization
+	void Start () 
+	{
+		cam = GetComponent<Camera> ();
+		aud = GameObject.FindGameObjectWithTag ("MM").GetComponent<AudioSource>();
+	}
+
+	public void Mute()
+	{
+		aud.enabled = false;
+	}
+
+	public void Unmute()
+	{
+		aud.enabled = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
